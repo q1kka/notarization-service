0c2a29508618db98967a213ee75d9d07fb6625ff
kaikka <kalehtim@jyu.fi>
2018-08-29 22:06:39 +0300

    add isEmpty util function

---
+++ src/utils/isEmpty.js
@@ -0,0 +1,15 @@
+/*
+ * Validation function for empty values
+ * (c) Kai-Markus Lehtimäki
+ */
+
+function isEmpty(value) {
+  return (
+    value === undefined ||
+    value === null ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0)
+  );
+}
+
+export default isEmpty;
